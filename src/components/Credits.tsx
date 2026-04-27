dd03ad8e340eb25b08ab2096783c9d3e2cee7838
adippe12 <adp1.998.pe@gmail.com>
2026-04-27 21:31:20 +0200

    Update Credits.tsx

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -21,7 +21,7 @@ const CREDITS = [
   { role: "Website Panles", name: "skiiryu", link: "https://vgen.co/skiiryu/shop"},
   { role: "Bit Badges", name: "PegaDesign", link: "https://www.etsy.com/shop/PegaDesign" },
   { role: "HUD League Overlay", name: "lunariachi", link: "https://x.com/lunariachi" },
-  { role: "PFP", name: "velirys", link: "https://x.com/velirys" },
+  { role: "PFP", name: "kiwiqwq0", link: "https://x.com/kiwiqwq0" },
   { role: "Banner", name: "TishyChanVT", link: "https://x.com/TishyChanVT" },
   { role: "Logo", name: "MikaLogo", link: "https://x.com/MikaLogo" }
 ];
