078849a23dd314f4c3a913ec40579262e0dc7e71
adippe12 <adp1.998.pe@gmail.com>
2026-09-16 23:05:04 +0200

    Update Credits.tsx

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -9,7 +9,6 @@ const CREDITS = [
   { role: "Chibi model, offline screen art, starting and ending scene, background in lobby scene art, panels & dolphin dance emote", name: "Lillimon", link: "https://x.com/_Lillimon_" },
   { role: "Background art in game scene", name: "icorasama", link: "https://x.com/icorasama" },
   { role: "Channel Points & Tier Flair", name: "ShoYukia", link: "https://www.etsy.com/it/shop/ShoYukia?ref=shop-header-name&listing_id=1852544810&from_page=listing" },
-  { role: "Badges", name: "nyunnya", link: "https://vgen.co/nyunnya/shop" },
   { role: "Milio & Lulu Emotes", name: "SViSHOP", link: "https://www.etsy.com/shop/SViSHOP#items" },
   { role: "Soraka & Yuumi Emotes", name: "cinamoncune", link: "https://ko-fi.com/cinamoncune" },
   { role: "Animated Emotes", name: "EmoteLab on steam", link: "" },
@@ -23,7 +22,7 @@ const CREDITS = [
   { role: "HUD League Overlay", name: "lunariachi", link: "https://x.com/lunariachi" },
   { role: "PFP", name: "Ifyoureally_art", link: "https://vgen.co/Ifyoureally_art" },
   { role: "Logo", name: "MikaLogo", link: "https://x.com/MikaLogo" },
-  { role: "PngTuber", name: "MeryMelon", link: "https://x.com/MeryMelon" },
+  { role: "PngTuber, Badges", name: "MeryMelon", link: "https://x.com/MeryMelon" },
   { role: "Street Girl Model", name: "kyoki_studio_II", link: "https://vgen.co/kyoki_studio_II" }
 ];
 
